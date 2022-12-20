f63ba4bc76029a3c877ee3e81996ba64492e0c6f
hotsix <codinggo@naver.com>
2022-12-21 00:52:45 +0900

    TodoList detach02

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import Header from "./components/Header";
 import Input from "./components/Input";
+import TodoList from "./components/TodoList";
 import "./App.css";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  // todos: todo라는 객체를 여러개 가지고 있는 배열!!
+  const [todos, setTodos] = useState(initialState);
   return (
     <div>
       <Header></Header>
@@ -16,6 +21,8 @@ function App() {
         <Input />
 
         {/* Todo 관련된 것 */}
+        <TodoList todos={todos} isActive={true} />
+        <TodoList todos={todos} isActive={false} />
       </main>
       <footer>푸터입니다.</footer>
     </div>
@@ -23,3 +30,24 @@ function App() {
 }
 
 export default App;
+
+const initialState = [
+  {
+    title: "제목1",
+    contents: "내용1",
+    isDone: false,
+    id: uuidv4(),
+  },
+  {
+    title: "제목2",
+    contents: "내용2",
+    isDone: true,
+    id: uuidv4(),
+  },
+  {
+    title: "제목3",
+    contents: "내용3",
+    isDone: false,
+    id: uuidv4(),
+  },
+];
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const TodoList = () => {
+// Props로 todos와 isActive를 가져옴.
+const TodoList = ({ todos, isActive }) => {
   return (
-    <div>TodoList</div>
-  )
-}
+    <>
+      {/* 해석: isActive라면 "할 일"이다. 그게 아니라면 "완료"이다. */}
+      <h4>{isActive ? "할 일" : "완료"}</h4>
+      {
+        // map 함수를 통해서 전체를 출력함.
+      }
+    </>
+  );
+};
 
-export default TodoList
+export default TodoList;
