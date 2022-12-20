467e1c9b1a244ed60e83c281bc62383dd12c9a43
hotsix <codinggo@naver.com>
2022-12-21 00:13:04 +0900

    Input detach01

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import Header from "./components/Header";
+import Input from "./components/Input";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <main>
+      <main
+        style={{
+          padding: "20px",
+          backgroundColor: "#fcba03",
+        }}
+      >
         {/* input 관련된 것 */}
-        <section>
-          <input />
-          <input />
-          <button>추가</button>
-        </section>
+        <Input />
+
         {/* Todo 관련된 것 */}
       </main>
       <footer>푸터입니다.</footer>
---
+++ src/components/Input.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <div>
+      <section>
+        제목: <input />
+        내용: <input />
+        <button>추가</button>
+      </section>
+    </div>
+  );
+};
+
+export default Input;
