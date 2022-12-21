6ddb2e2aa45607093ffb50064506c64c6b831977
hotsix <codinggo@naver.com>
2022-12-21 10:34:35 +0900

    TodoList todos03

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -6,9 +6,27 @@ const TodoList = ({ todos, isActive }) => {
     <>
       {/* 해석: isActive라면 "할 일"이다. 그게 아니라면 "완료"이다. */}
       <h4>{isActive ? "할 일" : "완료"}</h4>
-      {
-        // map 함수를 통해서 전체를 출력함.
-      }
+      {/* filter를 통해 isDone을 골라서 보여줌. 
+        해석: item중에 isDone이 isActive가 아닌 것만 나와주세요.
+        map 함수를 통해서 전체를 출력함. 19분08초 */}
+      {todos
+        .filter((item) => item.isDone === !isActive)
+        .map((item) => {
+          return (
+            <div
+              style={{
+                border: "1px solid black",
+              }}
+              key={item.id}
+            >
+              <h5>{item.title}</h5>
+              <p>{item.contents}</p>
+              {/* 해석: isActive라면 완료이다. 그게 아니라면 취소이다. */}
+              <button>{isActive ? "완료" : "취소"}</button>
+              <button>삭제</button>
+            </div>
+          );
+        })}
     </>
   );
 };
